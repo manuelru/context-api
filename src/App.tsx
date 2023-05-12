4eb46f024374efd6c6d3b6bae9682d3224534fd4
manuelru <manuelru26042000@gmail.com>
2023-05-11 20:40:54 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { MyContext } from "./MyContext";
+import MyComponent from "./MyComponent";
+
+
+function App() {
+  const [text, setText] = useState("");
+
+  return (
+    <div>
+      <MyContext.Provider value={{ text, setText }}>
+        <MyComponent />
+      </MyContext.Provider>
+    </div>
+  );
+}
+
+export default App;
